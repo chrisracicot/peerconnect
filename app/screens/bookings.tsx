42ca3a441db53b3e6e1872e6f2711181d39f8296
glconde <georgelouie.conde@edu.sait.ca>
2025-07-08 20:25:29 -0600

    remove bookings warning

--- app/screens/bookings.tsx
+++ app/screens/bookings.tsx
@@ -1,1 +1,12 @@
-import { useFormData } from '@context/FormContext';
+import { useFormData } from "@context/FormContext";
+
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function BookingsScreen() {
+  return (
+    <View>
+      <Text>Bookings Screen</Text>
+    </View>
+  );
+}
